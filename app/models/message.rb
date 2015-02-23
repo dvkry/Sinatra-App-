0314764db7cd0f38d4a02256188f43e064664d6a
David Vickery <david.vickery.23@gmail.com>
2015-02-23 20:55:02 +0000

    remove title

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ActiveRecord::Base
-  validates :title, presence: true, length: { minimum: 2 }
+
   validates :content, presence: true, length: { minimum: 2 }
   validates :author, presence: true, length: { minimum: 5 }
 
