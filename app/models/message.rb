bf78d7d90c6e0021c1f832b248dfe35d0712e61a
David Vickery <david.vickery.23@gmail.com>
2015-02-23 21:34:54 +0000

    add url

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,6 @@ class Message < ActiveRecord::Base
 
   validates :content, presence: true, length: { minimum: 2 }
   validates :author, presence: true, length: { minimum: 5 }
+#  validates :url, format: { with: /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/, message: "Not a valid url!" }
 
 end
